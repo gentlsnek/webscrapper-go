96464149e1e6f9f2b86624a584d6df707fd36e32
gentlsnek <erwinpimenta1644@gmail.com>
2025-05-31 10:53:56 +0530

    done

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
-func main(){
-	fmt.Println("Hello World!")
+func main() {
+	var filepath string
+	fmt.Println("Enter file path")
+	fmt.Scan(&filepath)
+
+	content, err := os.ReadFile(filepath)
+
+	if err != nil {
+		fmt.Println("Error reading file: ", err)
+		return
+	}
+
+	text := string(content)
+
+	websites := strings.Split(text, ",")
+
+	fmt.Println(websites)
+
 }
