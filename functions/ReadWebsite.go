6767b381151fd9f129ac146a0fef694c40dd7ba1
gentlsnek <erwinpimenta1644@gmail.com>
2025-05-31 19:34:55 +0530

    finished connecting http

--- functions/ReadWebsite.go
+++ functions/ReadWebsite.go
@@ -1,10 +1,31 @@
 package functions
 
 import (
-// "fmt"
-// "net"
+	"fmt"
+	"net/http"
+	//"strings"
+	//"time"
+	//"github.com/cilium/ebpf/link"
 )
 
 func ReadWebsite(sites []string) {
 
+	c := make(chan string)
+
+	for _, list := range sites {
+		go checkLink("http://"+list, c)
+	}
+
+}
+
+func checkLink(link string, c chan string) {
+	status, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println("could not establish connection to", err)
+		c <- link
+		return
+	}
+	fmt.Println(status)
+	c <- link
 }
